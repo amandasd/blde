cf7ac625133dad544d69454ad7b7c8f97790ea39
Amanda S. Dufek <amanda.dufek@gmail.com>
2021-04-22 22:11:32 -0300

    Update main.sh

--- sequential/main.sh
+++ sequential/main.sh
@@ -5,9 +5,9 @@ seq="$home/sequential"
 
 dimL=("0" "2" "5" "10")
 dimF=("0" "3" "5" "10")
-popL=("0" "32" "64" "128")
-popF=("0" "32" "128")
-genF=("0" "32" "128")
+popL=("0" "32" "64" "128" "256" "512")
+popF=("0" "32" "64" "128" "256" "512")
+genF=("0" "50" "100" "200" "300" "500")
 func=("0" "1001" "1002" "1003" "1004" "1005" "1006" "1007" "1008")
 
 cd $seq
@@ -20,14 +20,16 @@ for s in $(seq 1 1); do
    
       for d in $(seq 2 2); do
    
-         for pL in $(seq 1 2); do
+         for pL in $(seq 2 5); do
    
-            for pF in $(seq 1 2); do
+            for pF in $(seq 2 5); do
    
-               for gF in $(seq 1 2); do
+               for gF in $(seq 2 5); do
    
                   cd $seq
-                  ./main -popL ${popL[$pL]} -popF ${popF[$pF]} -genF ${genF[$gF]} -genL 100 -dimF ${dimF[$d]} -dimL ${dimL[$d]} -func ${func[$f]} -v 3 -F 0.8 -CR 0.9 -seed $(date +'%N') >> $home/output/seq_${func[$f]}_${popL[$pL]}_${popF[$pF]}_${genF[$gF]}_${dimL[$d]}x${dimF[$d]}
+		  echo $s ${popL[$pL]} ${popF[$pF]} ${genF[$gF]} ${dimF[$d]} ${dimL[$d]}
+                  ./main -popL ${popL[$pL]} -popF ${popF[$pF]} -genF ${genF[$gF]} -genL 10000 -dimF ${dimF[$d]} -dimL ${dimL[$d]} -func ${func[$f]} -v 3 -F 0.8 -CR 0.9 -seed $(date +'%N') >> $home/output/seq_${func[$f]}_${popL[$pL]}_${popF[$pF]}_${genF[$gF]}_${dimL[$d]}x${dimF[$d]}
+                  exit
    
                done
    
@@ -40,5 +42,3 @@ for s in $(seq 1 1); do
    done
 
 done
-
-
