e63236d05b949da09abc28cb20bd1b63eb1b5fa6
Amanda S. Dufek <amanda.dufek@gmail.com>
2024-09-18 14:49:55 -0700

    Changing 'data' to 'sdata'

--- src/blde.cc
+++ src/blde.cc
@@ -24,7 +24,7 @@ namespace { static struct t_data { int num_generation_leader; int population_lea
 #ifdef PROFILING
 double time_init; double time_seed; double time_initialization; double time_follower; double time_leader;
 #endif
-} data; };
+} sdata; };
 
 void blde_init( int argc, char** argv ) 
 {
@@ -52,27 +52,46 @@ void blde_init( int argc, char** argv )
    Opts.Process();
 
    // getting the results!
-   data.verbose = Opts.Bool.Get("-v");
+   sdata.verbose = Opts.Bool.Get("-v");
 
-   data.num_generation_leader = Opts.Int.Get("-gl");
-   data.population_leader_size = Opts.Int.Get("-pls");
+   sdata.num_generation_leader = Opts.Int.Get("-gl");
+   sdata.population_leader_size = Opts.Int.Get("-pls");
 
-   data.population_follower_size = Opts.Int.Get("-pfs");
+   sdata.population_follower_size = Opts.Int.Get("-pfs");
 
-   data.leader_dimension = Opts.Int.Get("-dl");
-   data.follower_dimension = Opts.Int.Get("-df");
+   sdata.leader_dimension = Opts.Int.Get("-dl");
+   sdata.follower_dimension = Opts.Int.Get("-df");
 
-   int r = floor( data.leader_dimension/2. );
-   int p = data.leader_dimension - r;
+   int r = floor( sdata.leader_dimension/2. );
+   int p = sdata.leader_dimension - r;
    int q; int s;
    if( Opts.String.Get("-function") == "1006" )
    {
-      q = floor( ( data.follower_dimension - r )/2. - EPS );
-      s = ceil(  ( data.follower_dimension - r )/2. + EPS );
+      if ( sdata.leader_dimension + sdata.follower_dimension == 5 )
+      {
+         r = 1;
+         p = 1;
+         q = 1;
+         s = 1;
+      } else if ( sdata.leader_dimension + sdata.follower_dimension == 10 )
+      {
+         q = 2;
+         s = 1;
+      } else if ( sdata.leader_dimension + sdata.follower_dimension == 20 )
+      {
+         q = 3;
+         s = 2;
+      } else if ( sdata.leader_dimension + sdata.follower_dimension == 64 )
+      {
+         q = 7;
+         s = 9;
+      }
+      //p = floor( ( sdata.follower_dimension - r )/2. - EPS );
+      //s = ceil(  ( sdata.follower_dimension - r )/2. + EPS );
    }
    else
    {
-      q = data.follower_dimension - r;
+      q = sdata.follower_dimension - r;
       s = 0.;
    }
 
@@ -97,21 +116,22 @@ void blde_init( int argc, char** argv )
    }
 
 #ifdef PROFILING
-   data.time_init = t_init.elapsed();
+   sdata.time_init = t_init.elapsed();
 #endif
 }
 
 int best_individual(int idx, real_t* fit_popL, real_t* fit_popLValoresF)
 {
    int idx_level_1 = 0; int idx_level_2 = 0;
-   for( int i = 1; i < data.population_leader_size; i++ )
+   for( int i = 1; i < sdata.population_leader_size; i++ )
    {
       // TODO: which option: #1 or #2?
       // The functions 1001, 1002, ..., 1008 are all minimization functions.
       // They do not have any restriction. 
       // Option #1
       //if( (fit_popL[i] > 0.) && (fit_popLValoresF[i] > 0.) ) {
-         if( (fit_popL[i] <= fit_popL[idx]) && (fit_popLValoresF[i] <= fit_popLValoresF[idx]) ) { idx = i; }
+      if( (fit_popL[i] <= fit_popL[idx]) ) { idx = i; }
+      //if( (fit_popL[i] <= fit_popL[idx]) && (fit_popLValoresF[i] <= fit_popLValoresF[idx]) ) { idx = i; }
       //}
       // Option #2
       //if( (fit_popL[i] <= fit_popL[idx_level_1]) ) { idx_level_1 = i; }
@@ -127,15 +147,15 @@ int best_individual(int idx, real_t* fit_popL, real_t* fit_popLValoresF)
 void blde_evolve()
 {
 #ifdef PROFILING
-   data.time_follower = 0.0;
-   data.time_leader   = 0.0;
+   sdata.time_follower = 0.0;
+   sdata.time_leader   = 0.0;
 #endif
 
 //#if ! defined( PROFILING )
-   real_t* popL = new real_t[data.population_leader_size * data.leader_dimension]; 
-   real_t* popLValoresF = new real_t[data.population_leader_size * data.follower_dimension];
-   real_t* fit_popL = new real_t[data.population_leader_size]; 
-   real_t* fit_popLValoresF = new real_t[data.population_leader_size]; 
+   real_t* popL = new real_t[sdata.population_leader_size * sdata.leader_dimension]; 
+   real_t* popLValoresF = new real_t[sdata.population_leader_size * sdata.follower_dimension];
+   real_t* fit_popL = new real_t[sdata.population_leader_size]; 
+   real_t* fit_popLValoresF = new real_t[sdata.population_leader_size]; 
 //#endif
 
 #ifdef PROFILING
@@ -143,7 +163,7 @@ void blde_evolve()
 #endif
    acc_seed();
 #ifdef PROFILING
-   data.time_seed = t_seed.elapsed();
+   sdata.time_seed = t_seed.elapsed();
 #endif
 
 #ifdef PROFILING
@@ -155,15 +175,15 @@ void blde_evolve()
    // popL and popLValoresF initialization
    // end
 #ifdef PROFILING
-   data.time_initialization = t_initialization.elapsed();
+   sdata.time_initialization = t_initialization.elapsed();
 #endif
 
    bool stop_fit = false; bool stop_stag = false; bool stop_eps = false;
    int idx = 0; int g; int stagnation_tolerance = 0;
-   int nEval_level_1 = data.population_leader_size; // acc_leader(g=0)
-   int nEval_level_2 = ( 2*data.population_follower_size*data.population_leader_size ) + ( data.population_leader_size ); // ( acc_follower(1) ) + ( acc_leader(g=0) )
-	for( g = 0; !stop_eps && ( g < data.num_generation_leader ); g++ )
-	//for( g = 0; ( g < data.num_generation_leader ) && ( nEval_level_1 + nEval_level_2 < nEval ) && !stop_fit && !stop_stag; g++ )
+   int nEval_level_1 = sdata.population_leader_size; // acc_leader(g=0)
+   int nEval_level_2 = ( 2*sdata.population_follower_size*sdata.population_leader_size ) + ( sdata.population_leader_size ); // ( acc_follower(1) ) + ( acc_leader(g=0) )
+	for( g = 0; !stop_eps && ( g < sdata.num_generation_leader ); g++ )
+	//for( g = 0; ( g < sdata.num_generation_leader ) && ( nEval_level_1 + nEval_level_2 < nEval ) && !stop_fit && !stop_stag; g++ )
    {  
 #ifdef PROFILING
       util::Timer t_follower;
@@ -172,7 +192,7 @@ void blde_evolve()
       // POPL_SIZE uLs run simultaneously, so at the end you have POPL_SIZE uFs 
       acc_follower( 0 );
 #ifdef PROFILING
-      data.time_follower += t_follower.elapsed();
+      sdata.time_follower += t_follower.elapsed();
 #endif
 
 #ifdef PROFILING
@@ -185,16 +205,16 @@ void blde_evolve()
 //#endif
                 );
 #ifdef PROFILING
-      data.time_leader += t_leader.elapsed();
+      sdata.time_leader += t_leader.elapsed();
 #endif
 
       // testa criterio de parada
       // start
-      nEval_level_1 += data.population_leader_size; // acc_leader
-      nEval_level_2 += ( 2*data.population_follower_size*data.population_leader_size ) + ( data.population_leader_size ); // ( acc_follower ) + ( acc_leader )
+      nEval_level_1 += sdata.population_leader_size; // acc_leader
+      nEval_level_2 += ( 2*sdata.population_follower_size*sdata.population_leader_size ) + ( sdata.population_leader_size ); // ( acc_follower ) + ( acc_leader )
 
 //#if ! defined( PROFILING )
-      printf( "\n[%d] %.12f :: %.12f :: %d :: %d :: %d :: %d :: %d", g, fit_popL[idx], fit_popLValoresF[idx], nEval_level_1, nEval_level_2, stop_fit, stop_stag, stop_eps ); 
+      //printf( "\n[%d] %.12f :: %.12f :: %d :: %d :: %d :: %d :: %d", g, fit_popL[idx], fit_popLValoresF[idx], nEval_level_1, nEval_level_2, stop_fit, stop_stag, stop_eps ); 
 		int idx_new = best_individual( idx, fit_popL, fit_popLValoresF );
 
       if( idx_new == idx ) stagnation_tolerance++;
@@ -206,34 +226,34 @@ void blde_evolve()
       // testa criterio de parada
       // end
 
-      if (data.verbose)
+      if (sdata.verbose)
       {
          printf( "\n[%d] %.12f :: %.12f :: %d :: %d :: %d :: %d :: %d", g, fit_popL[idx], fit_popLValoresF[idx], nEval_level_1, nEval_level_2, stop_fit, stop_stag, stop_eps ); 
          cout << "\n[Leader] ";
-         for( int j = 0; j < data.leader_dimension; j++ ){
-            cout << popL[idx + j * data.population_leader_size] << " ";
+         for( int j = 0; j < sdata.leader_dimension; j++ ){
+            cout << popL[idx + j * sdata.population_leader_size] << " ";
          }
          cout << "\n[Follower] ";
-         for( int j = 0; j < data.follower_dimension; j++ ){
-            cout << popLValoresF[idx + j * data.population_leader_size] << " ";
+         for( int j = 0; j < sdata.follower_dimension; j++ ){
+            cout << popLValoresF[idx + j * sdata.population_leader_size] << " ";
          }
          cout << endl;
       }
       double eps = 0.;
-      for( int j = 0; j < data.leader_dimension; j++ ){
-         eps += popL[idx + j * data.population_leader_size] * popL[idx + j * data.population_leader_size];
+      for( int j = 0; j < sdata.leader_dimension; j++ ){
+         eps += popL[idx + j * sdata.population_leader_size] * popL[idx + j * sdata.population_leader_size];
       }
       if (sqrt(eps) < 0.01) { stop_eps = true; }
 //#endif
 	}
-   if (data.verbose) printf( "\n" ); 
+   if (sdata.verbose) printf( "\n" ); 
 
 #ifdef PROFILING
-   printf("time_init: %lf, time_seed: %lf, time_initialization: %lf, time_follower: %lf, time_leader: %lf, gen: %d, stop_eps: %d\n", data.time_init, data.time_seed, data.time_initialization, data.time_follower/g, data.time_leader/g, g, stop_eps);
+   printf("time_init: %lf, time_seed: %lf, time_initialization: %lf, time_follower: %lf, time_leader: %lf, gen: %d, stop_eps: %d\n", sdata.time_init, sdata.time_seed, sdata.time_initialization, sdata.time_follower/g, sdata.time_leader/g, g, stop_eps);
 #endif
 
 //#if ! defined( PROFILING )
-   if (data.verbose){ 
+   if (sdata.verbose){ 
 	   idx = best_individual( idx, fit_popL, fit_popLValoresF );
       printf( "[%d] %.12f :: %.12f :: %d :: %d :: %d :: %d :: %d\n", g, fit_popL[idx], fit_popLValoresF[idx], nEval_level_1, nEval_level_2, stop_fit, stop_stag, stop_eps ); 
    }
